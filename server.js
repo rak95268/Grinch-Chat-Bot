3e347968bb3dab1da4aa78be3179d57e6a2b9a46
Kaimal, Ria Ajay <rkaimal3@gatech.edu>
2023-10-25 16:05:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,43 +1,21 @@
-const express = require('express');
-const axios = require('axios');
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: 
+    [{ 
+        //The system message, with the role "system," is used to set the initial context or behavior for the model.
+        role: "system", 
+        content: "You are a poet." 
+    },
+    {   //The user message, with the role "user," represents the message or query from the user. 
+        role: "user", 
+        content: "Hi,How are you?" 
+    }],
+    // the model we used
+    model: "gpt-3.5-turbo",
+  });
 
-const app = express();
+  console.log(completion.choices[0].message.content);
+}
 
-// Set EJS as the view engine
-app.set('view engine', 'ejs');
-
-// Middleware to parse incoming requests with JSON payloads
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-app.post('/ask', async (req, res) => {
-    const userMessage = req.body.message;
-
-    try {
-        const openaiResponse = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
-            prompt: userMessage,
-            max_tokens: 150
-        }, {
-            headers: {
-                'Authorization': `Bearer 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`,
-                'Content-Type': 'application/json'
-            }
-        });
-
-        const botReply = openaiResponse.data.choices[0].text.trim();
-        res.json({ reply: botReply });
-
-    } catch (error) {
-        console.error("Error querying OpenAI:", error);
-        res.status(500).send("An error occurred while fetching the response.");
-    }
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
-});
+//call the main() function
+main();
