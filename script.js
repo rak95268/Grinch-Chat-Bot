c24d966cbd4d390cc9d0ca1295498e9c348ee771
Yang, Yonseo <yonseoyang@gatech.edu>
2023-11-03 17:07:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const handleChat = () => {
 
      setTimeout(() => {
          // Display "Thinking..." message while waiting for the response
-         const incomingChatLi = createChatLi("Thinking...", "incoming");
+         const incomingChatLi = createChatLi("Stealing Christmas...", "incoming");
          chatbox.appendChild(incomingChatLi);
          chatbox.scrollTo(0, chatbox.scrollHeight);
          generateResponse(incomingChatLi);
